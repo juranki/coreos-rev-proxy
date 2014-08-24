83a45913f151b54c835ebaa66d3f800586f2f170
Juhani Ränkimies <juhani@juranki.com>
2014-08-24 21:51:10 +0300

    move destination determination to separate fun

--- src/Network/CoreOSRevProxy/Main.hs
+++ src/Network/CoreOSRevProxy/Main.hs
@@ -5,11 +5,19 @@ import Network (withSocketsDo)
 import Network.HTTP.Client (defaultManagerSettings, withManager)
 import Network.HTTP.ReverseProxy (ProxyDest (..), WaiProxyResponse (..), defaultOnExc, waiProxyTo)
 import Network.Wai.Handler.Warp (defaultSettings, runSettings, setPort)
+import Network.Wai (Request)
+import Network.Wai (rawPathInfo)
+import qualified Data.ByteString.Char8 as C8
+
+getDest :: Request -> IO WaiProxyResponse
+getDest req = do
+  C8.putStrLn $ rawPathInfo req
+  return $ WPRProxyDest $ ProxyDest "127.0.0.1" 8000
 
 main :: IO ()
 main =
     let settings = setPort 80 defaultSettings
     in withSocketsDo $ do
       withManager defaultManagerSettings $ \manager ->
-          runSettings settings $ waiProxyTo (const $ return $ WPRProxyDest $ ProxyDest "127.0.0.1" 8000) defaultOnExc manager
+          runSettings settings $ waiProxyTo getDest defaultOnExc manager
 
